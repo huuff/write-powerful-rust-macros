5a4d7b6d468cfb32045f789b7c8a92fae1031e82
Haf <haf@protonmail.ch>
2024-10-26 13:16:36 +0200

    feat(ch6): white box tests

--- ch6/builder-code/src/lib.rs
+++ ch6/builder-code/src/lib.rs
@@ -14,3 +14,47 @@ pub fn create_builder(item: TokenStream) -> TokenStream {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn builder_struct_name_should_be_present_in_output() {
+        let input = quote! {
+            struct StructWithNoFields {}
+        };
+
+        let actual = create_builder(input);
+
+        assert!(actual.to_string().contains("StructWithNoFieldsBuilder"));
+    }
+
+    #[test]
+    fn builder_struct_with_expected_methods_should_be_present_in_output() {
+        let input = quote! {
+            struct StructWithNoFields {}
+        };
+        let expected = quote! {
+            struct StructWithNoFieldsBuilder {}
+        };
+
+        let actual = create_builder(input);
+
+        assert_eq!(actual.to_string(), expected.to_string());
+    }
+
+    #[test]
+    fn assert_with_parsing() {
+        let input = quote! {
+            struct StructWithNoFields {}
+        };
+
+        let actual = create_builder(input);
+
+        let derived: DeriveInput = syn::parse2(actual).unwrap();
+        let name = derived.ident;
+
+        assert_eq!(name.to_string(), "StructWithNoFieldsBuilder");
+    }
+}
