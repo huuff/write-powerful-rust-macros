aacfdd6fe31cbe7e147f071b1b8a0dac870b4692
Haf <haf@protonmail.ch>
2024-11-06 17:02:02 +0100

    feat: chapter 7 exercise 1

--- ch4/make-public-macro/src/lib.rs
+++ ch4/make-public-macro/src/lib.rs
@@ -1,77 +1,39 @@
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::parse::Parse;
-use syn::{
-    parse_macro_input, DataStruct, DeriveInput, Fields, FieldsNamed, FieldsUnnamed, Token, Type,
-    Visibility,
-};
+use syn::{parse_macro_input, DataStruct, DeriveInput, Fields, FieldsNamed, FieldsUnnamed};
 use syn::{Data, DataEnum};
 
 #[proc_macro_attribute]
 pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(item as DeriveInput);
-    let name = ast.ident;
+    let mut ast = parse_macro_input!(item as DeriveInput);
 
     match ast.data {
         Data::Struct(DataStruct {
-            fields: Fields::Named(FieldsNamed { ref named, .. }),
+            fields: Fields::Named(FieldsNamed { ref mut named, .. }),
             ..
         }) => {
-            let builder_fields = named.iter().map(|f| {
-                let mut nu_field = f.clone();
-                nu_field.vis = syn::Visibility::Public(Token![pub](proc_macro2::Span::call_site()));
-                quote!(#nu_field)
-            });
-
-            quote! {
-               pub struct #name {
-                   #(#builder_fields,)*
-               }
+            for field in named {
+                field.vis = syn::Visibility::Public(Default::default());
             }
-            .into()
         }
         Data::Struct(DataStruct {
-            fields: Fields::Unnamed(FieldsUnnamed { ref unnamed, .. }),
+            fields:
+                Fields::Unnamed(FieldsUnnamed {
+                    ref mut unnamed, ..
+                }),
             ..
         }) => {
-            let builder_fields = unnamed
-                .iter()
-                .map(|f| syn::parse2::<UnnamedStructField>(f.to_token_stream()).unwrap());
-
-            quote! {
-                pub struct #name(#(#builder_fields,)*);
+            for field in unnamed {
+                field.vis = syn::Visibility::Public(Default::default());
             }
-            .into()
         }
-        Data::Enum(DataEnum { ref variants, .. }) => {
-            let variants_iter = variants.into_iter();
-            quote! {
-                pub enum #name {
-                    #(#variants_iter,)*
-                }
-            }
-            .into()
+        Data::Enum(DataEnum { .. }) => {
+            ast.vis = syn::Visibility::Public(Default::default());
         }
         _ => unimplemented!("not available for unions"),
     }
-}
 
-struct UnnamedStructField(Type);
-
-impl ToTokens for UnnamedStructField {
-    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        let ty = &self.0;
-        quote!(pub #ty).to_tokens(tokens)
-    }
-}
-
-impl Parse for UnnamedStructField {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let _vis: syn::Result<Visibility> = input.parse();
-        let ty: syn::Type = input.parse()?;
-
-        Ok(UnnamedStructField(ty))
-    }
+    ast.into_token_stream().into()
 }
 
 #[proc_macro_attribute]
