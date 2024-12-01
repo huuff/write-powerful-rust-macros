92838d8a70f155f574f27b7c37dfb879d89a68dc
Haf <haf@protonmail.ch>
2024-12-01 11:10:56 +0100

    refactor: removed some random experiments

--- ch4/make-public-macro/src/lib.rs
+++ ch4/make-public-macro/src/lib.rs
@@ -1,7 +1,8 @@
 use proc_macro::TokenStream;
-use quote::{quote, ToTokens};
-use syn::{parse_macro_input, DataStruct, DeriveInput, Fields, FieldsNamed, FieldsUnnamed};
-use syn::{Data, DataEnum};
+use quote::ToTokens;
+use syn::spanned::Spanned;
+use syn::Data;
+use syn::{parse_macro_input, DataStruct, DeriveInput, Fields, FieldsNamed};
 
 #[proc_macro_attribute]
 pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -16,74 +17,12 @@ pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
                 field.vis = syn::Visibility::Public(Default::default());
             }
         }
-        Data::Struct(DataStruct {
-            fields:
-                Fields::Unnamed(FieldsUnnamed {
-                    ref mut unnamed, ..
-                }),
-            ..
-        }) => {
-            for field in unnamed {
-                field.vis = syn::Visibility::Public(Default::default());
-            }
+        _ => {
+            return syn::Error::new(ast.span(), "only works on structs with named fields")
+                .to_compile_error()
+                .into()
         }
-        Data::Enum(DataEnum { .. }) => {
-            ast.vis = syn::Visibility::Public(Default::default());
-        }
-        _ => unimplemented!("not available for unions"),
     }
 
     ast.into_token_stream().into()
-}
-
-#[proc_macro_attribute]
-pub fn delete(_attr: TokenStream, _item: TokenStream) -> TokenStream {
-    let public_version = quote! {};
-    public_version.into()
-}
-
-#[proc_macro_attribute]
-pub fn prefix(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as DeriveInput);
-
-    let name = input.ident;
-    let vis = input.vis;
-    let attrs = input.attrs;
-
-    let fields = match input.data {
-        syn::Data::Struct(syn::DataStruct {
-            fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
-            ..
-        }) => named,
-        _ => panic!("prefix only works on structs with named fields"),
-    };
-
-    let fields = fields.into_iter().map(|field| {
-        if field
-            .attrs
-            .iter()
-            .any(|attr| attr.meta.path().is_ident("pfx"))
-        {
-            let field_attrs = field
-                .attrs
-                .iter()
-                .filter(|attr| !attr.meta.path().is_ident("pfx"));
-            let field_name = syn::Ident::new(
-                &format!("pfx_{}", field.ident.as_ref().unwrap()),
-                proc_macro2::Span::call_site(),
-            );
-            let field_ty = &field.ty;
-            quote!(#(#field_attrs)* #field_name: #field_ty)
-        } else {
-            quote!(#field)
-        }
-    });
-
-    quote! {
-        #(#attrs)*
-        #vis struct #name {
-            #(#fields,)*
-        }
-    }
-    .into()
 }
