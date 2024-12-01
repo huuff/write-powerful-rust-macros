5b88a1156eed6b788251d205de2d91d7a3db8966
Haf <haf@protonmail.ch>
2024-12-01 10:52:44 +0100

    refactor: very tiny refactor

--- ch8/builder-code/src/fields.rs
+++ ch8/builder-code/src/fields.rs
@@ -1,8 +1,8 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{punctuated::Punctuated, token::Comma, Field, Ident, Type};
+use syn::{punctuated::Punctuated, token::Comma, Field, Ident};
 
-use crate::utils::{create_builder_ident, create_field_struct_name};
+use crate::utils::{create_builder_ident, create_field_struct_name, get_name_and_type};
 
 pub fn original_struct_setters(
     fields: &Punctuated<Field, Comma>,
@@ -176,10 +176,4 @@ fn builder_for_final_field(
             }
         }
     }
-}
-
-fn get_name_and_type(f: &Field) -> (&Option<Ident>, &Type) {
-    let field_name = &f.ident;
-    let field_ty = &f.ty;
-    (field_name, field_ty)
 }
--- ch8/builder-code/src/utils.rs
+++ ch8/builder-code/src/utils.rs
@@ -1,5 +1,6 @@
 use proc_macro2::Ident;
 use quote::format_ident;
+use syn::{Field, Type};
 
 pub fn create_builder_ident(name: &Ident) -> Ident {
     format_ident!("{}Builder", name)
@@ -7,4 +8,10 @@ pub fn create_builder_ident(name: &Ident) -> Ident {
 
 pub fn create_field_struct_name(builder: &Ident, field: &Ident) -> Ident {
     format_ident!("{}Of{}", field, builder)
+}
+
+pub fn get_name_and_type(f: &Field) -> (&Option<Ident>, &Type) {
+    let field_name = &f.ident;
+    let field_ty = &f.ty;
+    (field_name, field_ty)
 }
