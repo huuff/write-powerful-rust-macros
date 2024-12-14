e8be413eaaea8cac83f3421399ffed60400d8635
Haf <haf@protonmail.ch>
2024-12-14 12:06:33 +0100

    refactor: alternative parsing for lambdas

--- ch9/src/main.rs
+++ ch9/src/main.rs
@@ -6,22 +6,30 @@ fn main() {
     }
 
     iac! {
-        lambda a_name
+        lambda ( name =  a_name)
     }
 
     iac! {
-        lambda my_hame mem 1024 time 15
+        lambda (
+            name = my_name,
+            mem = 1024,
+            time = 15,
+        )
     }
 
     iac! {
-        lambda name bucket uniquename
+        lambda (name = name) bucket uniquename
     }
 
     iac! {
-        bucket uniquename => lambda anothername
+        bucket uniquename => lambda (name = anothername)
     }
 
     iac! {
-        bucket b => lambda l mem 1024 time 15
+        bucket b => lambda (
+            name = l,
+            mem = 1024,
+            time = 15
+        )
     }
 }
